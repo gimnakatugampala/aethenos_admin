2b123c0ed95fc08c8cb570d28be7923098808016
gimnakatugampala <gimnakatugampala1@gmail.com>
2023-10-09 19:37:46 +0530

    2023_10_09_Admin

--- src/views/manage-admins/all-admins/AllAdmins.js
+++ src/views/manage-admins/all-admins/AllAdmins.js
@@ -2,7 +2,7 @@ import React, { useState ,useMemo } from 'react'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from 'react-bootstrap/Button';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Table from 'react-bootstrap/Table';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
@@ -13,11 +13,53 @@ import Form from 'react-bootstrap/Form';
 import Swal from 'sweetalert2';
 import { Player } from 'video-react';
 import MaterialTable from 'material-table';
+import Chip from '@mui/material/Chip';
 
 import 'video-react/dist/video-react.css'; // import css
 import './AllAdmins.css'
 
 const AllAdmins = () => {
+
+  const handledeactive = () =>{
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, Deactivate it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deactivated!',
+          'Admin has been Deactivated.',
+          'success'
+        )
+      }
+    })
+  }
+
+  const handleActive = () =>{
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, Active it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Activated!',
+          'Admin has been Activated.',
+          'success'
+        )
+      }
+    })
+  }
+
   return (
     <div>  
     <Card sx={{ minWidth: 275 }}>
@@ -27,20 +69,18 @@ const AllAdmins = () => {
       </Typography>
 
       <MaterialTable
-      title="Basic Search Preview"
+      title="Admins"
       columns={[
-        { title: 'Name', field: 'name' },
-        { title: 'Surname', field: 'surname' },
-        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-        {
-          title: 'Birth Place',
-          field: 'birthCity',
-          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-        },
+        { title: '#ID', field: 'id' },
+        { title: 'First Name', field: 'first_name' },
+        { title: 'Last Name', field: 'last_name' },
+        { title: 'Email', field: 'email' },
+        { title: 'Status', field: 'status' },
+        { title: 'Actions', field: 'actions' },
       ]}
       data={[
-        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+        { id: '001', first_name: 'Baran', last_name: 'John', email: 'john@gmail.com', status: <Chip label="Active" color="success" />, actions:<div className='d-flex'><Button onClick={handledeactive} className='mx-1' variant="contained"><CloseIcon /></Button><Button onClick={handleActive} className='mx-1' color="success" variant="contained"><CheckIcon /></Button></div>},
+        { id: '002', first_name: 'Baran', last_name: 'John', email: 'john@gmail.com',status: <Chip label="Inactive" color="error" /> , actions:<div className='d-flex'><Button onClick={handledeactive} className='mx-1' variant="contained"><CloseIcon /></Button><Button onClick={handleActive} className='mx-1' color="success" variant="contained"><CheckIcon /></Button></div>},
       ]}        
       options={{
         search: true
--- src/views/manage-courses/approve-lectures/ApproveLectures.js
+++ src/views/manage-courses/approve-lectures/ApproveLectures.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from 'react-bootstrap/Button';
+import Typography from '@mui/material/Typography';
+import Table from 'react-bootstrap/Table';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Swal from 'sweetalert2';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './ApproveLectures.css'
 
 const ApproveLectures = () => {
   return (
-    <div>ApproveLectures</div>
+    <div>  
+    <Card sx={{ minWidth: 275 }}>
+    <CardContent>
+    <Typography variant="h2" gutterBottom>
+        Approve Lectures
+      </Typography>
+
+      </CardContent>
+
+      </Card>
+    </div>
   )
 }
 

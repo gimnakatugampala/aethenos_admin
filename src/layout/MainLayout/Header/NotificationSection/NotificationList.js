a1129bfdaacd55c2cf6751012657fc898081ee84
surenfernand <141896367+surenfernand@users.noreply.github.com>
2024-08-20 16:58:06 +0530

    Suren_update_2024-08-20
    Updated Notification time

--- src/layout/MainLayout/Header/NotificationSection/NotificationList.js
+++ src/layout/MainLayout/Header/NotificationSection/NotificationList.js
@@ -46,6 +46,25 @@ const NotificationList = () => {
   const theme = useTheme();
   const [myNotifications, setmyNotifications] = useState([]);
 
+  const calculateTimeAgo = (dateString) => {
+    const now = new Date();
+    const date = new Date(dateString);
+    const seconds = Math.floor((now - date) / 1000);
+    let interval = Math.floor(seconds / 31536000);
+  
+    if (interval > 1) return `${interval} years ago`;
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) return `${interval} months ago`;
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) return `${interval} days ago`;
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) return `${interval} hours ago`;
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) return `${interval} minutes ago`;
+    return `${Math.floor(seconds)} seconds ago`;
+  };
+  
+
   const chipSX = {
     height: 24,
     padding: '0 6px'
@@ -171,7 +190,7 @@ const NotificationList = () => {
                     <Grid>
                       <Typography variant="caption">
                         <span className="d-flex justify-content-end">
-                          {moment(notification.notificationTime).startOf('hour').fromNow()}
+                          {calculateTimeAgo(notification.notificationTime)}
                         </span>
                       </Typography>
                     </Grid>

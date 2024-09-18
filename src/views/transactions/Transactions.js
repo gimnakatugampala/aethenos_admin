95c0012cc64a7235d90d9233576903bbe9a2b2a3
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-09-18 14:09:51 +0530

    transactions_init

--- src/views/transactions/Transactions.js
+++ src/views/transactions/Transactions.js
@@ -1,6 +1,10 @@
 import { GetAllTransactions } from 'api'
 import React from 'react'
 import { useEffect } from 'react'
+import MaterialTable from 'material-table'
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 const Transactions = () => {
 
@@ -10,7 +14,40 @@ const Transactions = () => {
     
 
   return (
-    <div>Transactions</div>
+    <div>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h2" gutterBottom>
+          Transactions
+          </Typography>
+
+       <MaterialTable
+      title=""
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+      ]}        
+      actions={[
+        {
+          icon: 'save',
+          tooltip: 'Save User',
+          onClick: (event, rowData) => alert("You saved " + rowData.name)
+        }
+      ]}
+    />
+     </CardContent>
+      </Card>
+    </div>
   )
 }
 
